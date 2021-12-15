04470656c87519d2465ccd19f2d7eea5b3b50c28
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-12-15 12:21:58 +0530

    Create Final Value of Variable After Performing Operations.cpp

---
+++ ARRAYS/Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+        int x=0;
+        int n=operations.size();
+        for(int i=0;i<n;i++)
+        {
+            if(operations[i]=="--X")
+                --x;
+            else if(operations[i]=="X--")
+                x--;
+            else if(operations[i]=="X++")
+                x++;
+            else
+                ++x;
+        }
+        return x;
+        
+    }
+};
