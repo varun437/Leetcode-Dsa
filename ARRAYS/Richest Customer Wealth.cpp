dbbede65b380c15fcd8098ca705c0d08df452f80
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-12-15 12:20:53 +0530

    Create  Richest Customer Wealth.cpp

---
+++ ARRAYS/Richest Customer Wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi=0;
+        int sum=0;
+        for(int i=0;i<accounts.size();i++)
+        {
+            sum=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                sum+=accounts[i][j];
+            }
+            maxi=max(maxi,sum);
+        }
+        return maxi;
+    }
+};
