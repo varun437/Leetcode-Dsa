4466ce701e9f6675cbe23572c9fdc9401ea0adb6
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-12-21 16:11:57 +0530

    Create Insertion Sort List.cpp

---
+++ LINKED-LISTS/Insertion Sort List.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode *start=new ListNode(0);
+        start->next=head;
+        ListNode *curr=head,*prev=start;
+        
+        while(curr)
+        {
+            if(curr->next && curr->next->val<curr->val)
+            {
+                while(prev->next && prev->next->val<curr->next->val)
+                    prev=prev->next;
+                ListNode *temp=prev->next;
+                prev->next=curr->next;
+                curr->next=curr->next->next;
+                prev->next->next=temp;
+                prev=start;
+                
+            }
+            else
+            {
+                curr=curr->next;
+            }
+        }
+        return start->next;
+    }
+};
