4cb694e9995c76cb83e4af58b9562bb515028cca
Malempati Varun <67381413+varun437@users.noreply.github.com>
2022-01-04 21:42:52 +0530

    Update Check if the Sentence Is Pangram.cpp

--- HASH-TABLE/Check if the Sentence Is Pangram.cpp
+++ HASH-TABLE/Check if the Sentence Is Pangram.cpp
@@ -13,3 +13,24 @@ public:
         return true;
     }
 };
+                   
+
+                (OR)
+
+class Solution {
+public:
+    bool checkIfPangram(string s) {
+        map<char,int>cntr;
+        int n=s.length();
+        
+        for(int i=0;i<n;i++){
+            cntr[s[i]]++;
+        }
+        for(char i='a';i<='z';i++){
+            if(cntr[i]==0)
+                return false;
+        }
+        return true;
+        
+    }
+};
